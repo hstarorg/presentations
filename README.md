eb30f0b1f9682aeb34104afb15ca01cc30772cce
Jay.M.Hu <jay.m.hu@newegg.com>
2017-08-01 11:02:13 +0800

    Add project build

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # presentations
 My html presentations.
+
+# How to start?
+
+```bash
+# Clone project
+git clone
+
+# Install deps
+npm i
+
+# Run
+npm run dev
+```
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp4');
+const browserSync = require('browser-sync').create();
+
+gulp.task('serve', done => {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  done();
+});
+
+gulp.task('reload', done => {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('watch', done => {
+  gulp.watch([
+    'index.html',
+    'slides/**/*',
+    'app/**/*'
+  ], gulp.series('reload'));
+  done();
+});
+
+gulp.task('default', gulp.parallel('serve', 'watch'));
