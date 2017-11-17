751d78e0732fbb3303a26662a4519d7808f12312
Hstarorg <hm910705@163.com>
2017-11-17 22:37:53 +0800

    Optimized for 16:9 screen.

--- slides/vue-those-things/index.js
+++ slides/vue-those-things/index.js
@@ -7,33 +7,42 @@ Reveal.initialize({
   transition: 'slide', // none/fade/slide/convex/concave/zoom
 
   // Optional reveal.js plugins
-  dependencies: [
-    {
+  dependencies: [{
       src: '../../reveal.js/lib/js/classList.js',
-      condition: function() {
+      condition: function () {
         return !document.body.classList;
       }
     },
     {
       src: '../../reveal.js/plugin/markdown/marked.js',
-      condition: function() {
+      condition: function () {
         return !!document.querySelector('[data-markdown]');
       }
     },
     {
       src: '../../reveal.js/plugin/markdown/markdown.js',
-      condition: function() {
+      condition: function () {
         return !!document.querySelector('[data-markdown]');
       }
     },
     {
       src: '../../reveal.js/plugin/highlight/highlight.js',
       async: true,
-      callback: function() {
+      callback: function () {
         hljs.initHighlightingOnLoad();
       }
     },
-    { src: '../../reveal.js/plugin/zoom-js/zoom.js', async: true },
-    { src: '../../reveal.js/plugin/notes/notes.js', async: true }
+    {
+      src: '../../reveal.js/plugin/zoom-js/zoom.js',
+      async: true
+    },
+    {
+      src: '../../reveal.js/plugin/notes/notes.js',
+      async: true
+    }
   ]
 });
+
+document.addEventListener('click', function (evt) {
+  console.log(evt.target);
+}, false);
